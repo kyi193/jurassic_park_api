7ef588182f0dde7cafa3e95b4313becb91f0707d
Kevin <kevin.yi93@gmail.com>
2021-01-18 22:51:53 -0500

    Add validation for adding dinosaurs to cage

--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -3,9 +3,22 @@ class Dinosaur < ApplicationRecord
   belongs_to :cage
   validates :name, presence: true
   validate :cage_occupancy_available
+  validate :cage_is_active
+  validate :dinosaur_breed_is_valid
 
   def cage_occupancy_available
     return if cage.max_capacity > cage.dinosaurs.count
     errors.add(:cage, "Cage is at maximum capacity")
+  end
+
+  def cage_is_active
+    return if cage.cage_status.name == "ACTIVE"
+    errors.add(:cage, "Cage status is DOWN")
+  end
+
+  def dinosaur_breed_is_valid
+    return if cage.dinosaurs.count == 0
+    return if (cage.dinosaurs[0].dinosaur_breed.diet_type.name == dinosaur_breed.diet_type.name)
+    errors.add(:cage, "Cannot mix herbivores with carnivores in same cage")
   end
 end
