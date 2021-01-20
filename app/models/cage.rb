05da44c244db9d48e29d31ab84cd4ae5fc311fa5
Kevin <kevin.yi93@gmail.com>
2021-01-20 01:00:10 -0500

    Add validation for carnivores breed and powering down cages

--- app/models/cage.rb
+++ app/models/cage.rb
@@ -4,4 +4,11 @@ class Cage < ApplicationRecord
   validates :max_capacity, presence: true
   scope :filter_by_max_capacity, -> (max_capacity) { where max_capacity: max_capacity }
   scope :filter_by_cage_status, -> (cage_status) { joins(:cage_status).where(cage_status:{name: cage_status}) }
+  validate :cage_is_empty_when_power_is_off
+
+  def cage_is_empty_when_power_is_off
+    if (cage_status.name == "DOWN" and dinosaurs.count > 0)
+      errors.add(:cage, "Cannot power cage down when dinosaurs are present")
+    end
+  end
 end
--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -5,6 +5,7 @@ class Dinosaur < ApplicationRecord
   validate :cage_occupancy_available
   validate :cage_is_active
   validate :dinosaur_breed_is_valid
+  validate :carnivore_breed_is_valid
   scope :filter_by_breed, -> (dinosaur_breed) { joins(:dinosaur_breed).where(dinosaur_breed:{name: dinosaur_breed})}
 
   def cage_occupancy_available
@@ -21,5 +22,12 @@ class Dinosaur < ApplicationRecord
     return if cage.dinosaurs.count == 0
     return if (cage.dinosaurs[0].dinosaur_breed.diet_type.name == dinosaur_breed.diet_type.name)
     errors.add(:cage, "Cannot mix herbivores with carnivores in same cage")
+  end
+
+  def carnivore_breed_is_valid
+    return if cage.dinosaurs.count == 0
+    return if dinosaur_breed.diet_type.name == "Herbivore"
+    return if (cage.dinosaurs[0].dinosaur_breed.name == dinosaur_breed.name)
+    errors.add(:cage, "Cannot mix carnivores of different breeds")
   end
 end
